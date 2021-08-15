967a5ba1ca9aa9ffec4510a4f88df03ded1fb477
Re-Creation Developers ADMIN <recreationdevelopers@gmail.com>
2021-08-15 13:35:49 +0530

    added UI to add songs and listen to it

--- app.py
+++ app.py
@@ -19,6 +19,26 @@ from libs.db_sqlite import SqliteDatabase
 from libs.reader_file import FileReader
 ####################################################################################################
 
+############### From recognize-from-microphone.py###################################################
+
+import argparse
+import sys
+from argparse import RawTextHelpFormatter
+from itertools import zip_longest as izip_longest
+
+import numpy as np
+from termcolor import colored
+
+import libs.fingerprint as fingerprint
+from libs.config import get_config
+from libs.db_sqlite import SqliteDatabase, SQLITE_MAX_VARIABLE_NUMBER
+from libs.reader_microphone import MicrophoneReader
+from libs.visualiser_console import VisualiserConsole as visual_peak
+from libs.visualiser_plot import VisualiserPlot as visual_plot
+
+# from libs.db_mongo import MongoDatabase
+####################################################################################################
+
 class UiWindow(qtw.QMainWindow):
 
     def __init__(self, *args, **kwargs):
@@ -27,7 +47,7 @@ class UiWindow(qtw.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        # self.ui.selectSourceFolderBtn.clicked.connect(self.sourceFolderCmd)
+        self.ui.listenAndSearchBtn.clicked.connect(self.listenAndSearchCmd)
         self.ui.addFingerprintsBtn.clicked.connect(self.fingerprintCmd)
 
     def fingerprintCmd(self):
@@ -98,16 +118,207 @@ class UiWindow(qtw.QMainWindow):
 
         print('end')
 
+    def listenAndSearchCmd(self):
+
+        ################ Taken the code from recognize-from-microphone.py#################
+        def align_matches(matches):
+            diff_counter = {}
+            largest = 0
+            largest_count = 0
+            song_id = -1
+
+            for tup in matches:
+                sid, diff = tup
+
+                if diff not in diff_counter:
+                    diff_counter[diff] = {}
+
+                if sid not in diff_counter[diff]:
+                    diff_counter[diff][sid] = 0
+
+                diff_counter[diff][sid] += 1
+
+                if diff_counter[diff][sid] > largest_count:
+                    largest = diff
+                    largest_count = diff_counter[diff][sid]
+                    song_id = sid
+
+            songM = db.get_song_by_id(song_id)
+
+            nseconds = round(float(largest) / fingerprint.DEFAULT_FS *
+                            fingerprint.DEFAULT_WINDOW_SIZE *
+                            fingerprint.DEFAULT_OVERLAP_RATIO, 5)
+
+            return {
+                "SONG_ID": song_id,
+                "SONG_NAME": songM[1],
+                "CONFIDENCE": largest_count,
+                "OFFSET": int(largest),
+                "OFFSET_SECS": nseconds
+            }
+
+
+        def grouper(iterable, n, fillvalue=None):
+            args = [iter(iterable)] * n
+            return (filter(None, values)
+                    for values in izip_longest(fillvalue=fillvalue, *args))
+
+
+        def find_matches(samples, Fs=fingerprint.DEFAULT_FS):
+            hashes = fingerprint.fingerprint(samples, Fs=Fs)
+            return return_matches(hashes)
+
+
+        def return_matches(hashes):
+            mapper = {}
+            for hash, offset in hashes:
+                mapper[hash.upper()] = offset
+            values = mapper.keys()
+
+            for split_values in map(list, grouper(values, SQLITE_MAX_VARIABLE_NUMBER)):
+                # @todo move to db related files
+                query = """
+            SELECT upper(hash), song_fk, offset
+            FROM fingerprints
+            WHERE upper(hash) IN (%s)
+        """
+                query = query % ', '.join('?' * len(split_values))
+
+                x = db.executeAll(query, split_values)
+                matches_found = len(x)
+
+                if matches_found > 0:
+                    msg = '   ** found %d hash matches (step %d/%d)'
+                    print(colored(msg, 'green') % (
+                        matches_found,
+                        len(split_values),
+                        len(values)
+                    ))
+                else:
+                    msg = '   ** not matches found (step %d/%d)'
+                    print(colored(msg, 'red') % (len(split_values), len(values)))
+
+                for hash_code, sid, offset in x:
+                    # (sid, db_offset - song_sampled_offset)
+                    if isinstance(offset, bytes):
+                        # offset come from fingerprint.py and numpy extraction/processing
+                        offset = np.frombuffer(offset, dtype=np.int)[0]
+                    yield sid, offset - mapper[hash_code]
+
+
+        config = get_config()
+
+        db = SqliteDatabase()
+
+        # parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+        # parser.add_argument('-s', '--seconds', nargs='?')
+        # args = parser.parse_args()
+
+        # if not args.seconds:
+        #     parser.print_help()
+        #     sys.exit(0)
+
+        # seconds = int(args.seconds)
+
+        seconds = 5 # Take 5 seconds of input audio (making it fixed)
+
+        chunksize = 2 ** 12  # 4096
+        channels = 2  # int(config['channels']) # 1=mono, 2=stereo
+
+        record_forever = False
+        visualise_console = bool(config['mic.visualise_console'])
+        visualise_plot = bool(config['mic.visualise_plot'])
+
+        reader = MicrophoneReader(None)
+
+        reader.start_recording(seconds=seconds,
+                            chunksize=chunksize,
+                            channels=channels)
+
+        self.ui.outputMessageLabel.setText('Recording started...')
+        msg = ' * started recording..'
+        print(colored(msg, attrs=['dark']))
+
+        while True:
+            bufferSize = int(reader.rate / reader.chunksize * seconds)
+
+            for i in range(0, bufferSize):
+                nums = reader.process_recording()
+
+                if visualise_console:
+                    msg = colored('   %05d', attrs=['dark']) + colored(' %s', 'green')
+                    print(msg % visual_peak.calc(nums))
+                else:
+                    msg = '   processing %d of %d..' % (i, bufferSize)
+                    print(colored(msg, attrs=['dark']))
+
+            if not record_forever:
+                break
+
+        if visualise_plot:
+            data = reader.get_recorded_data()[0]
+            visual_plot.show(data)
+
+        reader.stop_recording()
+
+        self.ui.outputMessageLabel.setText('Recording stopped')
+        msg = ' * recording has been stopped'
+        print(colored(msg, attrs=['dark']))
+
+        data = reader.get_recorded_data()
+
+        msg = ' * recorded %d samples'
+        print(colored(msg, attrs=['dark']) % len(data[0]))
+
+        # reader.save_recorded('test.wav')
+
+        Fs = fingerprint.DEFAULT_FS
+        channel_amount = len(data)
+
+        result = set()
+        matches = []
+
+        for channeln, channel in enumerate(data):
+            # TODO: Remove prints or change them into optional logging.
+            msg = '   fingerprinting channel %d/%d'
+            print(colored(msg, attrs=['dark']) % (channeln + 1, channel_amount))
+
+            matches.extend(find_matches(channel))
+
+            msg = '   finished channel %d/%d, got %d hashes'
+            print(colored(msg, attrs=['dark']) % (channeln + 1,
+                                                channel_amount, len(matches)))
+
+        total_matches_found = len(matches)
+
+        print('')
+
+        if total_matches_found > 0:
+            msg = ' ** totally found %d hash matches'
+            print(colored(msg, 'green') % total_matches_found)
+
+            song = align_matches(matches)
+
+            self.ui.outputMessageLabel.setText('Track name: ' + song['SONG_NAME'] +'\n' +
+                                                'Track ID: ' + str(song['SONG_ID']) +'\n' +
+                                                'Confidence of Search: ' + str(song['CONFIDENCE']))
+
+            msg = ' => song: %s (id=%d)\n'
+            msg += '    offset: %d (%d secs)\n'
+            msg += '    confidence: %d'
+
+            print(colored(msg, 'green') % (song['SONG_NAME'], song['SONG_ID'],
+                                        song['OFFSET'], song['OFFSET_SECS'],
+                                        song['CONFIDENCE']))
+
+        else:
+            self.ui.outputMessageLabel.setText("No matches found. Try again.")
+
+            msg = ' ** not matches found at all'
+            print(colored(msg, 'red'))
+
 if __name__ == '__main__':
     # print("This script is not supposed to be run directly from here")
-
-    # If we want to run uiDesign.py along with its functionalities in this code, un-comment the following:
-    # app = QtWidgets.QApplication(sys.argv)
-    # MainWindow = QtWidgets.QMainWindow()
-    # ui = Ui_MainWindow()
-    # ui.setupUi(MainWindow)
-    # MainWindow.show()
-    # sys.exit(app.exec_())
 
     app = qtw.QApplication([])
     widget = UiWindow()
